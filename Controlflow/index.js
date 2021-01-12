943b1666b000ba27d0deaa62101d77ac58e4da41
rhmerin <rhmerinit@gmail.com>
2021-01-12 20:29:21 +0600

    Added switch & case

--- Controlflow/index.js
+++ Controlflow/index.js
@@ -1,5 +1,7 @@
 //Control flow in javascript
 
+//if & else
+
 let hour = 12;
 
 if (hour >= 6 && hour < 12){
@@ -11,3 +13,19 @@ else if(hour >= 12 && hour < 18){
 else{
     console.log("Good evening")
 }
+
+//switch & case
+
+let user = 'guest';
+ 
+switch(user){
+    case 'verified':
+    console.log('Verified user')
+    break;
+
+    case 'guest':
+    console.log('Guest user')
+    break;
+    default :
+    console.log('Unknown user')
+};
